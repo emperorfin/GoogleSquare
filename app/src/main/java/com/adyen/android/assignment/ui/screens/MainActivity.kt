9c51d57dd83bd838f038334a342c11364091a90c
Francis Nwokelo (emperorfin) <emperorfin4@gmail.com>
2022-03-03 04:18:12 +0100

    - [CREATED] Added a yet to complete main screen. - [FIXED] Changed Kotlin Gradle Plugin version from 1.6.10 to 4.1.1 to fix Gradle sync issue. - [FIXED] Fixed Gradle build issues associated with starting the main screen's activity component.

---
+++ app/src/main/java/com/adyen/android/assignment/ui/screens/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.adyen.android.assignment.ui.screens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.adyen.android.assignment.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
